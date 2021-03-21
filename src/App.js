9fb4121609af00627f47cc0d83fffd6479f8c970
Packman700 <53169727+Packman700@users.noreply.github.com>
2021-03-21 12:38:58 +0100

    [feature] -edit note

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
         allTodos:[{text:'test_todo1', isCompleted:true, id:'1123'}, {text:'test_todo2', isCompleted:true, id:'11223'}],
         navigationState:'all',
         newTodo:'',
+        editTodoData:{isCompleted:false, isEdited:false}
     }
 
     changeNavigationState = (event) => {
@@ -28,8 +29,9 @@ class App extends React.Component {
     addNewTodo = (event) => {
         event.preventDefault()
         const newTodoText = this.state.newTodo
+        const isCompleted = this.state.editTodoData.isCompleted
         if (newTodoText) {
-            const newTodo = {text: newTodoText, isCompleted: false, id:makeID(10)}
+            const newTodo = {text: newTodoText, isCompleted: isCompleted, id:makeID(10)}
             const todosToFormat = JSON.parse(JSON.stringify(this.state.allTodos)) // Deep copy state.allTodos
             todosToFormat.push(newTodo)
 
@@ -37,7 +39,8 @@ class App extends React.Component {
                 return ({
                     ...lastState,
                     allTodos: todosToFormat,
-                    newTodo: ''
+                    newTodo: '',
+                    editTodoData:{isEdited:false, isCompleted: false}
                 })
             })
         } else {
@@ -46,7 +49,7 @@ class App extends React.Component {
         }
     }
 
-    deleteTodo = (event)=>{
+    deleteTodo = (event) => {
         const {id} = event.target.dataset
         const todosToFormat = JSON.parse(JSON.stringify(this.state.allTodos)) // Deep copy state.allTodos
         const indexToDelete = returnMachIndex(todosToFormat, id)
@@ -55,7 +58,7 @@ class App extends React.Component {
         this.setState({allTodos: todosToFormat})
     }
 
-    updateCheckState = (event)=>{
+    updateCheckState = (event) => {
         const {id} = event.target.dataset
         const todosToFormat = JSON.parse(JSON.stringify(this.state.allTodos)) // Deep copy state.allTodos
         const machIndex = returnMachIndex(todosToFormat, id)
@@ -64,8 +67,25 @@ class App extends React.Component {
         this.setState({allTodos: todosToFormat})
     }
 
+    editTodo = (event) => {
+        if (this.state.editTodoData.isEdited)
+            alert("First end edit selected todo")
+        else{
+            const {id} = event.target.dataset
+            const allTodos = this.state.allTodos // Deep copy state.allTodos
+            const machIndex = returnMachIndex(allTodos, id)
+            const todoIsCompleted = allTodos[machIndex].isCompleted
+            const todoText = allTodos[machIndex].text
+            this.setState({
+                    newTodo:todoText,
+                    editTodoData:{isCompleted:todoIsCompleted, isEdited:true}
+            })
+            this.deleteTodo(event)
+        }
+    }
+
     render() {
-        const todoList = this.state.allTodos.filter((todoItem)=>{
+        const todoList = this.state.allTodos.filter((todoItem) => {
             if (this.state.navigationState === 'active' && todoItem.isCompleted === false)
                 return(todoItem)
             if (this.state.navigationState === 'completed' && todoItem.isCompleted === true)
@@ -76,9 +96,10 @@ class App extends React.Component {
             .map((todoItem)=>{
                 return (
                     <TodoListItems
-                        data={todoItem}
-                        deleteTodo={this.deleteTodo}
-                        updateCheckState={this.updateCheckState}
+                        data = {todoItem}
+                        deleteTodo = {this.deleteTodo}
+                        updateCheckState = {this.updateCheckState}
+                        editTodo = {this.editTodo}
                     />
                 )
             })
--- src/components/TodoListItems.js
+++ src/components/TodoListItems.js
@@ -1,5 +1,6 @@
 import React from "react";
 import binIcon from "icons/delete_outline-black-18dp.svg" // in future add react-relative-path
+import editIcon from "icons/edit.svg" // in future add react-relative-path
 
 function TodoListItems(props){
     const text = props.data.text
@@ -18,10 +19,22 @@ function TodoListItems(props){
                 {text}
             </label>
             <img
+                width="18px" height="18px"
                 src={binIcon}
                 onClick={props.deleteTodo}
                 data-id={id}
+                alt="bin icon"
             />
+
+            <img
+                width="18px" height="18px"
+                src={editIcon}
+                onClick={props.editTodo}
+                data-id={id}
+                alt="edit icon"
+            />
+
+
         </div>
     )
 }
